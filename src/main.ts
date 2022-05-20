f3331c6bc8eefc9ec7db88eee367c1c58265a41d
josanma <j.sanma@hotmail.com>
2022-05-19 23:25:05 -0400

    config server http2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { createServer, Server, server } from 'spdy';
+import ServerOptions = server.ServerOptions;
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const expressApp = express();
+
+  const spdyOpts: ServerOptions = {
+    key: readFileSync('./cert/test.key'),
+    cert: readFileSync('./cert/test.crt'),
+  };
+
+  const server: Server = createServer(spdyOpts, expressApp);
+
+  const app: NestApplication = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
+  await app.init();
+  await server.listen(3000);
 }
+
 bootstrap();
